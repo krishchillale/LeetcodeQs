e9d0bf767965ec2ebe3475c56aaca9f5476cb1da
krishchillale <chillale.krish24@vit.edu>
2026-08-17 09:45:06 +0530

    BackSpacing Both strings and checking if found an # and checking whether they are equal or not

---
+++ Q844.java
@@ -0,0 +1,27 @@
+public class Q844 {
+        public boolean backspaceCompare(String s, String t) {
+            StringBuilder s1 = new StringBuilder("");
+            StringBuilder s2 = new StringBuilder("");
+            for(int i=0;i<s.length();i++){
+                if(s.charAt(i)=='#') {
+                    if (s1.length()>0){
+                        s1.deleteCharAt(s1.length() - 1);
+                }
+                }
+                else{
+                    s1.append(s.charAt(i));
+                }
+            }
+            for(int i=0;i<t.length();i++){
+                if(t.charAt(i)=='#'){
+                    if(s2.length()>0) {
+                        s2.deleteCharAt(s2.length() - 1);
+                    }
+                    }
+                else{
+                    s2.append(t.charAt(i));
+                }
+            }
+            return (s1.toString().equals(s2.toString())) ? true : false;
+        }
+}
