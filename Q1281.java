bdeffd4a7a0d26e3feddcd9bd99280cfe466493b
krishchillale <chillale.krish24@vit.edu>
2026-08-20 09:12:46 +0530

    Subtract the Product and Sum of Digits of an Integer

---
+++ Q1281.java
@@ -0,0 +1,12 @@
+public class Q1281 {
+    public int subtractProductAndSum(int n) {
+        int product=1,sum=0;
+        int temp = n;
+        while(temp>0){
+            product*=temp%10;
+            sum+=temp%10;
+            temp/=10;
+        }
+        return product-sum;
+    }
+}
