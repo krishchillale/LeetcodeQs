c7a98a99a98dcbcfc43e541fdbe7122148e6e786
krishchillale <chillale.krish24@vit.edu>
2026-08-20 09:09:00 +0530

    Nearest Drone Indexing using manhattan distance

---
+++ Q4024.java
@@ -0,0 +1,17 @@
+public class Q4024 {
+    public int nearestDrone(int[][] drones, int[] target) {
+        int x = target[0];
+        int y = target[1];
+        int min = Integer.MAX_VALUE,index=-1;
+        for(int i=0;i<drones.length;i++){
+            int manhattan = Math.abs(x-drones[i][0])+Math.abs(x-drones[i][1]);
+            if(manhattan<=drones[i][2]){
+                if(min>manhattan){
+                    min=manhattan;
+                    index=i;
+                }
+            }
+        }
+        return index;
+    }
+}
