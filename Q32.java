6a0318d189c3969881d6dd19b325509835109c9d
krishchillale <chillale.krish24@vit.edu>
2026-08-25 10:10:59 +0530

    Longest Valid Parentheses Solved using Stack (Hard Questionn Explaination saved in GoodQs)

--- Q32.java
+++ Q32.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.Stack;
 
 public class Q32 {
+    public int longestValidParentheses(String s) {
+        int count=0,max=0;
+        int n = s.length();
+        Stack<Integer> s1 = new Stack<>();
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='('){
+                s1.push(i);
+                count++;
+            }
+            else if(s.charAt(i)==')'&&s1.isEmpty()){
+                max=Math.max(count,max);
+                count=0;
+            }
+            else{
+                count++;
+                s1.pop();
+                if(s1.isEmpty()){
+                    max=Math.max(max,count);
+                }
+                else{
+                    max=Math.max(max,i-s1.peek());
+                }
+            }
+        }
+        if(!s1.isEmpty()){
+            count=n-1-s1.pop();
+        }
+        max=Math.max(max,count);
+        return max;
+    }
 }
