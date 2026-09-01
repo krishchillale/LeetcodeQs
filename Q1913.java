8dfaa880b6988aa07cd54fa4fd8f95d55978d30b
krishchillale <chillale.krish24@vit.edu>
2026-09-01 09:57:31 +0530

    Maximum Product Difference Between Two Pairs

---
+++ Q1913.java
@@ -0,0 +1,23 @@
+public class Q1913 {
+    public int maxProductDifference(int[] nums) {
+            int max1=Integer.MIN_VALUE,max2=Integer.MIN_VALUE;
+            int min1=Integer.MAX_VALUE,min2=Integer.MIN_VALUE;
+            for(int i=0;i<nums.length;i++){
+                if(max1<nums[i]){
+                   max2=max1;
+                   max1=nums[i];
+                }
+                else if(max2<nums[i]){
+                    max2=nums[i];
+                }
+                if(min1>nums[i]){
+                    min2=min1;
+                    min1=nums[i];
+                }
+                else if(min2>nums[i]){
+                    min2=nums[i];
+                }
+            }
+            return (max1*max2)-(min1*min2);
+    }
+}
