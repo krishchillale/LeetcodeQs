58284cfd216ad0bfa8f7c1004dbc20f089ce571a
krishchillale <chillale.krish24@vit.edu>
2026-09-10 19:33:56 +0530

    Count Distinct Integers with numbers and their reverse

--- Q2442.java
+++ Q2442.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
 
 public class Q2442 {
+    public int countDistinctIntegers(int[] nums) {
+        int n = nums.length;
+        int count=0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(!map.containsKey(nums[i])){
+                count++;
+                map.put(nums[i],1);
+            }
+            int temp=nums[i];
+            int rev=0;
+            while(temp>0){
+                rev = rev*10 +temp%10;
+                temp/=10;
+            }
+            if(!map.containsKey(rev)){
+                count++;
+                map.put(rev,1);
+            }
+        }
+        return count;
+    }
 }
